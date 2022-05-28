2e216fc2a2063ec4c7373abb9f0dbc158eccb2a4
financieras <financieras@gmail.com>
2022-05-28 20:56:20 +0200

    suma

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        vals = request.form.getlist('misvalores')
+        val1 = float(vals[0])
+        val2 = float(vals[1])
+        result = val1 + val2
+    else:
+        val1 = ''
+        val2 = ''
+        result = ''
+    return render_template('index.html', val1=val1, val2=val2, result=result)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
