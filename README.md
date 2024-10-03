4ac44cb2650d6826879aad2f405bb3cb50d56144
financieras <financieras@gmail.com>
2024-10-03 13:24:34 +0200

    approachs

--- README.md
+++
@@ -1,2 +0,0 @@
-# roll-dice
-Website to roll dice. It uses a Flask server and the logic is coded in Python.
--- game_logic.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Juego:
-    def __init__(self, num_caras=6):
-        self.num_caras = num_caras
-
-    def hacer_tirada(self):
-        return random.randint(1, self.num_caras)
-
