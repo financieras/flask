ac7c250167eebea459834f1bec74035342d096fa
financieras <financieras@gmail.com>
2024-10-04 10:49:44 +0200

    avanzando

--- approach6/static/js/script.js
+++ approach6/static/js/script.js
@@ -1,6 +1,18 @@
-fetch('/')
-  .then(response => response.json())
-  .then(data => {
-    const resultadoElement = document.getElementById('resultado');
-    resultadoElement.textContent = `El resultado es: ${data.resultado}`;
-  });
+const form = document.querySelector('form');
+const resultadoElement = document.getElementById('resultado');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const numCaras = formData.get('num_caras');
+
+  fetch('/')
+    .then(response => response.json())
+    .then(data => {
+      resultadoElement.innerHTML = `El resultado es: ${data.resultado}`;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+});
